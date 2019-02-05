0010d95da3c3ccd52b53e7f7468ce6c552987c6f
Sugandh Kumar Chaudhary <sugandh.kumar4@gmail.com>
2019-02-05 19:46:24 +0530

    First Commit - Added Spring Boot Project

---
+++ NewsScraper/src/main/java/com/news/scraper/NewsScraper/NewsScraperApplication.java
@@ -0,0 +1,14 @@
+package com.news.scraper.NewsScraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsScraperApplication.class, args);
+	}
+
+}
+
